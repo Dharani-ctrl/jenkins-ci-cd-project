4f2c0a285723df0960b24cf822bfa6839b54df93
Dharani-ctrl <sridhardharanidharan444@gmail.com>
2026-01-31 15:30:34 +0530

    Serverside Mongo to env to mongo  localhost Update recommit

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URI =
-  process.env.MONGO_URI || "mongodb://mongo:27017/skillmatch";
+  process.env.MONGO_URI || "mongodb://localhost:27017/skillmatch";
 
 mongoose
   .connect(MONGO_URI)
